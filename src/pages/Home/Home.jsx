2cba9089acc9da69ee9f7ce8256d974a78c7fe45
LuanOrlandelli <luanorlandelli0112@gmail.com>
2026-07-04 20:03:45 -0300

    Criando Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 function Home() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    async function carregarProdutos() {
+      try {
+        const response = await api.get("/produtos");
+        setProdutos(response.data);
+      } catch (error) {
+        console.error("Erro ao carregar produtos:", error);
+      }
+    }
+
+    carregarProdutos();
+  }, []);
+
   return (
     <div>
       <h1>GamersPlay Store</h1>
-      <p>Home funcionando!</p>
+
+      {produtos.map((produto) => (
+        <div key={produto.id}>
+          <h3>{produto.nome}</h3>
+          <p>{produto.descricao}</p>
+          <p>R$ {produto.preco}</p>
+        </div>
+      ))}
     </div>
   );
 }
